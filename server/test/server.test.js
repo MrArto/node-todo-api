6fbc16154ba36aa93402379ee8fd9afc732e5df0
Arto Kir <artavazd1980@gmail.com>
2019-04-11 14:08:03 +0900

    test the gote/todo

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,11 +4,24 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+
+
+const todos = [{
+  text: "First text todo"
+}, {
+  text: "Second text todo"
+}];
+
+
+
+
+
 beforeEach((done) => {
-Todo.remove({}).then(() => {
-  done()
-})
+Todo.deleteMany({}).then(() => {
+  return Todo.insertMany(todos)
+}).then(() => done());
 });
+
 
 describe('Post /todos', () => {
 it('should create new todo', (done) => {
@@ -27,8 +40,8 @@ request(app)
   };
 
   Todo.find().then((todos) => {
-    expect(todos.length).toBe(1);
-    expect(todos[0].text).toBe(text);
+    expect(todos.length).toBe(3);
+    expect(todos[2].text).toBe(text);
     done();
   }).catch((e) => done(e));
 });
@@ -46,12 +59,25 @@ it('should not create todo with invaled data', (done) => {
       };
 
       Todo.find().then((todos) => {
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(2);
         done();
       }).catch((e) => done(e));
     });
-})
+});
 
+});
+
+
+describe('Get/todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todos.length).toBe(2);
+    })
+    .end(done);
+  });
 });
 
 
